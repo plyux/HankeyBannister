c92a7bd2a24451460d2a8804e072cfb81a3e1123
plyux <lenin.jun@gmail.com>
2014-02-04 08:59:27 +0200

    last

---
+++ Hankey/Hankey/login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hankey
+{
+    public class login
+    {
+        public string username;
+        public string password;
+        public string provider;
+        public login(string name, string pass, string prov)
+        {
+            username = name;
+            password = pass;
+            provider = prov;
+        }
+        public bool verificate(string a)
+        {
+            if ((a.Contains("@gmail.com")) || (a.Contains("@mail.ru")) || (a.Contains("@ukr.net")))
+                return true;
+            else return false;
+        }
+    }
+}
